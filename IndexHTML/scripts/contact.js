62d1bc732b95b378cd62e17239e912582fa8e83d
Rayane <rchaabane20@gmail.com>
2023-11-07 15:59:45 +0100

    mise a jour des fichiers

--- IndexHTML/scripts/contact.js
+++ IndexHTML/scripts/contact.js
@@ -1,3 +1,5 @@
+
+
 // // Gestion des formulaires, des événements et de la logique métier.
 // // Vous devrez implémenter la logique métier détaillée décrite dans le cahier des charges.
 
---
+++ IndexHTML/scripts/reglement.js
@@ -0,0 +1,20 @@
+
+
+// function sectionLi(){
+//     const ulList = document.querySelectorAll("ul > li");
+//     for (let k = 0; k <ulList.length;k++) {
+//         // ulList[k].style.listStyleType="circle";
+//         ulList[k].style.fontFamily = "Arial, Helvetica, sans-serif";
+//         ulList[k].style.fontSize = "25px";
+
+        
+//     }
+//     // console.log(ulList);
+// }
+
+// // Cette fonction sera exécutée lorsque la page sera entièrement chargée
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     sectionLi();
+ 
+// });
